d77768ae7d48a5fd544bc30c43c30d3dc5804394
bABCDd <rlatkdrbs6845@gmail.com>
2025-07-25 20:58:56 +0900

    feat: SceneChange

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("potal"))
+        {
+            SceneManager.LoadScene("MiniGameScene");
+        }
+    }
+}
