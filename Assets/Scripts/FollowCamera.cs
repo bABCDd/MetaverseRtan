da0e035092e12c28334682f2ea92eee31a157c50
bABCDd <rlatkdrbs6845@gmail.com>
2025-07-25 16:06:19 +0900

    feat: FollowCamera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ // The target to follow
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+    float xCamera;
+    float yCamera;
+    float zCamera;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(target == null)
+        {
+            return;
+        }
+        xCamera = target.position.x;
+        yCamera = target.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = target.position;
+        transform.position = new Vector3(targetPosition.x, targetPosition.y, target.position.z - 10);
+    }
+}
